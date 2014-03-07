2eeb00d078f6a94e842b5b4bcbae3116cac81e57
sfeek <sfeek@yahoo.com>
2014-03-07 00:08:04 -0500

    Read Commit

---
+++ nerdz_server.rb
@@ -0,0 +1,362 @@
+#!/usr/bin/ruby 
+
+$debug=false
+
+##################
+#  nerdz server	 #	
+##################
+
+# Reply to ping packets
+def pong(client,cmd)
+    sleep 2
+	client.puts "pong\n"
+end
+
+# Register a new user
+def register(client,cmd,redis)
+	begin
+		# Make sure input is clean
+		if cmd[1] == nil
+			client.puts "failed"
+			return nil
+		end
+		if cmd[2] == nil
+			client.puts "failed"
+			return nil
+		end
+
+		key=cmd[1]
+		data=cmd[2]
+
+        # Add key tag
+        key = "KEY-" + key
+
+		# Check if already registered
+		if redis.exists(key) == true 
+			client.puts "already_registered"
+			return nil
+		end
+
+		# Register new user
+		if redis.set(key, data) != "OK"
+			client.puts "failed"
+			return nil
+		end
+
+		# Set expire to 30 days
+		if redis.expire(key, "2592000") != true
+			client.puts "failed"
+			return nil
+		end			
+
+		# Success!
+		client.puts "registered"
+		return 0
+	rescue Exception => msg
+		puts msg if $debug
+		client.puts "failed"		
+		return nil
+	end	
+end
+
+# Send to a client mailbox
+def send(client,cmd,redis)
+    begin
+		# Make sure input is clean
+		if cmd[1] == nil
+			client.puts "failed"
+			return nil
+		end
+
+		key=cmd[1]
+	
+        # Add key tag
+        keykey = "KEY-" + key
+
+        # Query Redis and send the public key to back to the client
+        rdata = redis.get(keykey) 
+        if rdata != nil
+            client.puts rdata
+        else
+        	client.puts "nouser"
+			return nil
+        end    
+        
+        # Wait for client to respond with data to be added to the database
+        cmd=client.readline.strip.split('|')
+
+        # Make sure input is clean
+		if cmd[0] != "send2"
+			client.puts "failed"
+			return nil
+		end
+ 		if cmd[1] == nil
+			client.puts "failed"
+			return nil
+		end
+        if cmd[2] == nil
+			client.puts "failed"
+			return nil
+        end
+
+		key=cmd[1]
+        data=cmd[2]
+	
+        # Add key tags
+        keykey = "KEY-" + key
+        keymsg = "MSG-" + key
+
+		# Add message to user's list
+		if redis.lpush(keymsg, data) == nil
+			client.puts "failed"
+			return nil
+		end
+
+		# Set message to expire in 30 days
+		if redis.expire(keymsg, "2592000") != true
+			client.puts "failed"
+			return nil
+		end		
+
+ 		# Success!
+		client.puts "sent"
+		return 0
+    rescue Exception => msg
+		puts msg if $debug
+		client.puts "failed"		
+		return nil
+    end	
+end
+
+# Read from client mailbox
+def read(client,cmd,redis)
+    begin
+		# Make sure input is clean
+		if cmd[1] == nil
+			client.puts "failed"
+			return nil
+		end
+
+		key=cmd[1]
+	
+        # Add key tags
+        keykey = "KEY-" + key
+        keymsg = "MSG-" + key
+
+		# Reset user key expire to 30 days
+		if redis.expire(keykey, "2592000") != true
+			client.puts "failed"
+			return nil
+		end	
+
+        # Query Redis and see if mailbox is empty, if so leave quickly 
+        len = redis.llen(keymsg) 
+        if len == 0
+           	client.puts "nomessage"
+			return nil
+        end 
+
+        # Query Redis and get the public key for the user 
+        rdata = redis.get(keykey) 
+        if rdata == nil
+           	client.puts "nouser"
+			return nil
+        end    
+    
+        # Make a public key from the users key 
+        pub_key = key_from_Base64(rdata)
+        
+        # Generate a random string and hash and keep for later
+        string = generate_random_key(30)
+        hash_string = hash_data(string)
+
+        # Encrypt with RSA public key
+        enc_rsa = encrypt_RSA_public(string,pub_key)
+    
+        # Send to client for verification
+        client.puts enc_rsa
+
+        # Wait for response from client
+        response = client.gets.strip
+
+        # Check response
+        if response == hash_string then 
+            client.puts "accepted"
+        else
+            client.puts "rejected"
+            return nil
+        end
+        
+        # Collect the messages from mailbox
+        messages = ""
+        
+        while (msg=redis.lpop(keymsg)) != nil do
+            messages.concat(msg+"|")
+        end
+        
+        # Send them to the client
+        client.puts messages
+     rescue Exception => msg
+		puts msg if $debug
+		client.puts "failed"		
+		return nil
+    end	
+end   
+
+# Unregister a user
+def unregister(client,cmd,redis)
+    begin
+		# Make sure input is clean
+		if cmd[1] == nil
+			client.puts "failed"
+			return nil
+		end
+
+		key=cmd[1]
+	
+        # Add key tag
+        keykey = "KEY-" + key
+        keymsg = "MSG-" + key
+
+        # Query Redis and get the public key for the user 
+        rdata = redis.get(keykey) 
+        if rdata == nil
+           	client.puts "nouser"
+			return nil
+        end    
+    
+        # Make a public key from the users key 
+        pub_key = key_from_Base64(rdata)
+        
+        # Generate a random string and hash and keep for later
+        string = generate_random_key(30)
+        hash_string = hash_data(string)
+
+        # Encrypt with RSA public key
+        enc_rsa = encrypt_RSA_public(string,pub_key)
+    
+        # Send to client for verification
+        client.puts enc_rsa
+
+        # Wait for response from client
+        response = client.gets.strip
+
+        # Check response
+        if response == hash_string then
+            # Delete the public key and messages
+		    redis.del(keykey) 
+            redis.del(keymsg)
+
+            client.puts "deleted"
+        else
+            client.puts "rejected"
+            return nil
+        end
+    rescue Exception => msg
+		puts msg if $debug
+		client.puts "failed"		
+		return nil
+    end	
+end   
+        
+	
+
+#####################
+#  Main entry point #
+#####################
+
+require 'socket'
+require 'redis'
+require 'base64'
+require_relative './public_encrypt'
+
+puts "Usage: nerdz_server <listen port> <redis hostname> <redis port>"
+puts "Ctrl-C to Stop Nerdz Server"
+
+# Sanitize command line parameters
+if ARGV[0]==nil
+	ARGV[0]='5150'              
+end
+if ARGV[1]==nil
+	ARGV[1]='localhost'
+end
+if ARGV[2]==nil
+	ARGV[2]='6379'
+end
+begin
+	lport=ARGV[0].strip.to_i
+	rhostname=ARGV[1].strip
+	rport=ARGV[2].strip.to_i
+rescue Exception => msg
+    puts msg if $debug
+	puts "Bad Parameter Specified!"
+	exit 1
+end
+
+# Connect with redis
+begin
+	redis = Redis.new(:host => rhostname, :port => rport, :db => 15)
+    if redis.set("ping", "pong") == nil
+        exit 1
+    end
+	puts "Connected to Redis Server"
+rescue Exception => msg
+	puts msg if $debug
+	puts "Redis Server Connection Failed!"
+	exit 1
+end
+
+# Open port for listening
+begin
+	server = TCPServer.open(lport)  
+	puts "Nerdz Server Listening on #{lport}"
+rescue Exception => msg
+	puts msg if $debug
+	puts "Unable to Listen on Port #{lport}, ABORTING!"
+	exit 1
+end
+
+# Begin threaded client accept
+begin
+	while(true)                      
+	  Thread.start(server.accept) do |client|
+		puts "Connection from #{client.peeraddr[2]}" if $debug
+		begin    
+			while (true) 
+				cmd=client.readline.strip.split('|')
+			
+				case cmd[0]
+				when "ping"
+					pong(client,cmd)
+				when "register"
+					if register(client,cmd,redis) == nil
+						break
+					end
+                when "send1"
+                    if send(client,cmd,redis) == nil
+                        break
+                    end
+                when "read1"
+                    if read(client,cmd,redis) == nil
+                        break;
+                    end
+                when "unregister"
+                    if unregister(client,cmd,redis) == nil
+                        break;
+                    end                
+				else
+					break
+				end
+			end
+			client.close
+		rescue Exception => msg
+			puts msg if $debug
+			client.close
+		end                
+	  end
+	end
+rescue Interrupt
+    # Exit due to Ctrl-C
+	puts "\nClosing Port #{lport}"
+	exit 0
+end
---
+++ public_encrypt.rb
@@ -0,0 +1,205 @@
+#!/usr/bin/ruby -w
+
+require 'openssl'
+require 'base64'
+
+
+# Encrypt string using AES256 with another persons public key
+# Input: string to be encrypted and public key
+# Output: string containing encrypted data:key:IV in base64 encoding
+def encrypt_public(string,key)
+    begin
+        # Encrypt with 256 bit AES with CBC
+        cipher = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
+
+        # We are encypting
+        cipher.encrypt 
+
+        # The OpenSSL library will generate random keys and IVs
+        cipher.key = random_key = cipher.random_key
+        cipher.iv = random_iv = cipher.random_iv
+
+        # Encrypt the data
+        encrypted_data = cipher.update(string)
+        encrypted_data << cipher.final
+
+        # Create public key        
+        public_key = OpenSSL::PKey::RSA.new(key)
+
+        # Encrypt our AES key and IV
+        encrypted_key = public_key.public_encrypt(random_key)
+        encrypted_iv = public_key.public_encrypt(random_iv)
+
+        # Creat Output string
+        output=Base64.strict_encode64(encrypted_data)+":"+Base64.strict_encode64(encrypted_key)+":"+Base64.strict_encode64(encrypted_iv)
+    rescue Exception => msg
+        puts msg if $debug
+        return nil
+    end
+
+    return output
+end
+
+# Decrypt using my private key
+# Input Base64 encoded string data:key:IV and private key
+# Output plain text string
+def decrypt_public(string,key)
+    begin
+
+        # Split apart data, key, and IV
+        data = string.split(":")
+        
+        # Decode from string back to binary format
+        encrypted_data=Base64.strict_decode64(data[0])
+        encrypted_key=Base64.strict_decode64(data[1])
+        encrypted_iv=Base64.strict_decode64(data[2])
+
+        # Create cipher object
+        cipher = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
+
+        # We are decrypting
+        cipher.decrypt
+
+        # Set the key and IV
+        cipher.key = key.private_decrypt(encrypted_key)
+        cipher.iv = key.private_decrypt(encrypted_iv)
+
+        # Perform the decryption and output
+        decrypted_data = cipher.update(encrypted_data)
+        decrypted_data << cipher.final
+
+    rescue Exception => msg
+        puts msg if $debug
+        return nil
+    end
+
+    return decrypted_data
+end
+
+# Hash data and return base64 encoded string
+def hash_data(data)
+	begin
+		sha256 = OpenSSL::Digest::SHA256.new
+		string = Base64.encode64(sha256.digest(data)).strip
+	rescue Exception => msg
+		puts msg if $debug
+		return nil
+	end
+	return string
+end
+
+# Create RSA key from Base64 string
+def key_from_Base64(string)
+	begin
+		key = OpenSSL::PKey::RSA.new Base64.strict_decode64(string)
+	rescue Exception => msg
+		puts msg if $debug
+		puts "Key Decode Error"
+		return nil
+	end
+	
+	return key
+end
+
+# Read public key from pem file and return as Base64 string
+def read_pub_key(username)
+	begin 
+		pubkeyname = File.expand_path("#{$path}/#{username}_public_key.pem")
+		
+		file = File.open(pubkeyname, "rb")
+		key = file.read
+	rescue Exception => msg
+		puts msg if $debug
+		puts "Public Key Read Failed!"
+		return nil
+	ensure
+  		file.close unless file == nil
+	end	
+	
+	return Base64.strict_encode64(key)
+end
+
+# Read private key from pem file and return as usable key
+def read_prv_key(username)
+	begin 
+		prvkeyname = File.expand_path("#{$path}/#{username}_priv_key.pem")
+		
+		prv_key=OpenSSL::PKey::RSA.new File.read prvkeyname
+	rescue Exception => msg
+		puts msg if $debug
+		puts "Private Key Read Failed!"
+		return nil
+	end
+	return prv_key
+end
+
+# Create key pair and store in files
+def make_keys(username)
+	begin
+        # New key
+		key = OpenSSL::PKey::RSA.new 2048
+
+        # Get the file paths
+		pubkeyname = File.expand_path("#{$path}/#{username}_public_key.pem")
+		prvkeyname = File.expand_path("#{$path}/#{username}_priv_key.pem")
+
+        # Encrypt private key and save both keys to disk
+		open pubkeyname, 'w' do |io| io.write key.public_key.to_pem end
+		cipher = OpenSSL::Cipher.new 'AES-128-CBC'
+		key_secure = key.export cipher
+		open prvkeyname, 'w' do |io|
+	  		io.write key_secure
+		end
+	rescue Exception => msg
+		puts msg if $debug
+		puts "Key Pair Create Failed!"
+		return nil
+	end
+    return 0
+end
+
+# Encrypt short string with RSA Public Key
+def encrypt_RSA_public(string,key)
+    begin
+        encrypted_string = Base64.strict_encode64(key.public_encrypt(string))
+    rescue Exception => msg
+		puts msg if $debug
+        return nil
+    end
+    return encrypted_string
+end
+
+# Decrypt short string with RSA Private Key
+def decrypt_RSA_public(string,key)
+    begin
+        decrypted_string = (key.private_decrypt(Base64.strict_decode64(string)))
+    rescue Exception => msg
+		puts msg if $debug
+        return nil
+    end
+    return decrypted_string
+end
+
+# Generate a random key and return as Base64
+def generate_random_key(size)
+    begin
+        string=Base64.strict_encode64(OpenSSL::Random.random_bytes(size))
+    rescue Exception => msg
+		puts msg if $debug
+        return nil
+    end
+    return string   
+end
+
+
+
+
+
+
+
+
+
+
+    
+    
+
