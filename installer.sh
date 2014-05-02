ea10a11ec85bab5c0c7b4321f55569b87f7c2d3d
Shane <shane@StarGazerWKS.(none)>
2014-05-02 15:42:52 -0400

    Daemonize Nerdz

--- installer.sh
+++ installer.sh
@@ -3,4 +3,5 @@
 CWD=$(pwd)
 ln -s $CWD/nerdz.rb /usr/bin/nerdz
 ln -s $CWD/nerdz_server.rb /usr/bin/nerdz_server
+ln -s $CWD/nerdz_control.rb /usr/bin/nerdz_control
 
---
+++ nerdz_control.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'daemons'
+
+Daemons.run('nerdz_server.rb')
+
